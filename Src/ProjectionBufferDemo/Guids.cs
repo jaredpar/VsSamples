98b6014bfa80b815cb2de49ac0f7b324d855f592
Jared Parsons <jaredpparsons@gmail.com>
2013-01-11 16:49:00 -0800

    Initial creation of the ProjectionBufferDemo package

---
+++ Src/ProjectionBufferDemo/Guids.cs
@@ -0,0 +1,15 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace JaredPar.ProjectionBufferDemo
+{
+    static class GuidList
+    {
+        public const string guidProjectionBufferDemoPkgString = "0d6c3e4c-e607-43b5-8ce7-8a6c7f07127d";
+        public const string guidProjectionBufferDemoCmdSetString = "1c4dda70-5d74-4ac8-9619-84fe10636305";
+        public const string guidToolWindowPersistanceString = "9deee938-cb7c-4f5a-90f4-6c70d3ac07bf";
+
+        public static readonly Guid guidProjectionBufferDemoCmdSet = new Guid(guidProjectionBufferDemoCmdSetString);
+    };
+}
---
+++ Src/ProjectionBufferDemo/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace JaredPar.ProjectionBufferDemo
+{
+    static class PkgCmdIDList
+    {
+        public const uint cmdidCreateProjection =        0x100;
+        public const uint cmdidProjectionViewer =    0x101;
+
+    };
+}
